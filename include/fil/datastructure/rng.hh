f82d10b3971b0b7cecbe42492847d05665833bcd
FyS <ballandFyS@gmail.com>
2020-07-25 00:10:51 +0200

    implement rng

--- include/fil/datastructure/rng.hh
+++ include/fil/datastructure/rng.hh
@@ -24,12 +24,79 @@
 #ifndef FIL_RNG_HH
 #define FIL_RNG_HH
 
+#include <type_traits>
+#include <memory>
+#include <random>
+#include <chrono>
+
+#include <fmt/format.h>
+
 namespace fil {
 
 class rng {
 
+public:
+	explicit rng(std::uint32_t seed = 0)
+			:_mt(std::make_unique<std::mt19937>()), _seed(seed)
+	{
+		if (!seed) {
+			std::random_device rd;
 
+			if (rd.entropy() != 0.0) {
+				_seed = rd();
+			}
+			else {
+				_seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+			}
+		}
+		_mt->seed(_seed);
+	}
 
+	template<typename Type>
+	[[nodiscard]] Type generate_in_range(Type rA, Type rB)
+	{
+		static_assert(std::is_integral_v<Type> || std::is_floating_point_v<Type>);
+		if constexpr (std::is_integral_v<Type>) {
+			std::uniform_int_distribution<Type> distribution(rA, rB);
+			return distribution(*_mt);
+		}
+		else if constexpr (std::is_floating_point_v<Type>) {
+			std::uniform_real_distribution<Type> distribution(rA, rB);
+			return distribution(*_mt);
+		}
+	}
+
+	[[nodiscard]] std::uint32_t get_seed() const { return _seed; }
+
+private:
+
+	std::unique_ptr<std::mt19937> _mt;
+	std::uint32_t _seed;
+};
+
+class uuid_generator {
+public:
+	explicit uuid_generator(const std::string& salt, std::uint32_t seed = 0)
+			:_rng(seed)
+	{
+		for (char c : salt) {
+			_salt += std::uint64_t(c);
+		}
+	}
+
+	std::string generate_uuid(std::uint32_t max_size = 16u)
+	{
+		auto uuid = fmt::format("{:x}{:x}{:x}", _salt, _id, _rng.generate_in_range(10000, 1000000));
+		if (uuid.size() > max_size) {
+			uuid.resize(max_size);
+		}
+		return uuid;
+	}
+
+private:
+	rng _rng;
+	std::uint64_t _salt{};
+	std::uint64_t _id{};
 };
 
 }
---
+++ tests/rng_testcase.cpp
@@ -0,0 +1,58 @@
+// MIT License
+//
+// Copyright (c) 2019 Quentin Balland
+// Repository : https://github.com/FreeYourSoul/FyS
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+//         of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+//         to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//         copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+//         copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//         AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include <catch2/catch.hpp>
+#include <fil/datastructure/rng.hh>
+
+TEST_CASE("rng_test_case", "[rng]")
+{
+	const std::uint32_t seed = 42;
+
+	SECTION("rng") {
+		fil::rng rnf(seed);
+
+		CHECK(42 == rnf.get_seed());
+
+		CHECK(380824 == rnf.generate_in_range(10000, 1000000));
+		CHECK(798641 == rnf.generate_in_range(10000, 1000000));
+		CHECK(951283 == rnf.generate_in_range(10000, 1000000));
+		CHECK(191615 == rnf.generate_in_range(10000, 1000000));
+		CHECK(734732 == rnf.generate_in_range(10000, 1000000));
+		CHECK(781956 == rnf.generate_in_range(10000, 1000000));
+
+	} // End section : rng
+
+	SECTION("uuid generator") {
+		fil::uuid_generator uuid_gen("FyS", seed);
+
+		CHECK("11205cf98" == uuid_gen.generate_uuid());
+		CHECK("1120c2fb1" == uuid_gen.generate_uuid());
+		CHECK("1120e83f3" == uuid_gen.generate_uuid());
+		CHECK("11202ec7f" == uuid_gen.generate_uuid());
+		CHECK("1120b360c" == uuid_gen.generate_uuid());
+		CHECK("1120bee84" == uuid_gen.generate_uuid());
+
+	} // End section :
+
+} // End TestCase : rng_test_case
+

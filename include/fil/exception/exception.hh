b026cc2b977b32b4f4fbe4cc69b056b5eb0d4732
FyS <ballandFyS@gmail.com>
2020-08-06 00:21:05 +0200

    finished implementation of rocksdb (to be tested)

---
+++ include/fil/exception/exception.hh
@@ -0,0 +1,57 @@
+// MIT License
+//
+// Copyright (c) 2020 Quentin Balland
+// Repository : https://github.com/FreeYourSoul/FyS
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+//         of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+//         to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//         copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+//         copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//         AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#ifndef FIL_INCLUDE_FIL_KV_DB_KV_DB_EXCEPTION_HH
+#define FIL_INCLUDE_FIL_KV_DB_KV_DB_EXCEPTION_HH
+
+#include <stdexcept>
+#include <system_error>
+
+#include <fmt/format.h>
+
+namespace fil {
+
+namespace except_cat {
+struct db : public std::error_category {
+   [[nodiscard]] const char* name() const noexcept override { return "fil::exception::db"; }
+   [[nodiscard]] std::string message(int I) const override {
+	  return fmt::format("Database Exception Category : {}", name());
+   }
+};
+
+struct fil : public std::error_category {
+   [[nodiscard]] const char* name() const noexcept override { return "fil::exception"; }
+};
+}// namespace except_cat
+
+class exception : public std::runtime_error {
+ public:
+   exception(const std::error_code& ec, const std::string& what) : std::runtime_error(what), _ec(ec) {}
+
+ private:
+   std::error_code _ec;
+};
+
+}// namespace fil
+
+#endif//FIL_INCLUDE_FIL_KV_DB_KV_DB_EXCEPTION_HH
--- internal/src/kv_db/kv_rocksdb.cpp
+++ internal/src/kv_db/kv_rocksdb.cpp
@@ -1,12 +1,15 @@
 #include <rocksdb/options.h>
-#include <rocksdb/utilities/transaction.h>
 #include <rocksdb/utilities/optimistic_transaction_db.h>
+#include <rocksdb/utilities/transaction.h>
 
 #include <fmt/format.h>
 
+#include <fil/exception/exception.hh>
 #include <fil/kv_db/kv_rocksdb.hh>
 
 namespace fil {
+
+std::error_code put_error_code() { return std::error_code(0, except_cat::db {}); }
 
 kv_rocksdb::transaction::transaction(kv_rocksdb& db) {
    rocksdb::WriteOptions write_options;
@@ -16,37 +19,66 @@ kv_rocksdb::transaction::transaction(kv_rocksdb& db) {
 }
 
 std::string kv_rocksdb::transaction::get(const std::string& key) {
+   std::string result;
+   rocksdb::ReadOptions opt;
 
-}
-
-
-bool kv_rocksdb::transaction::set(const key_value& to_add) {
-
-}
-
-bool kv_rocksdb::transaction::multi_set(const std::vector<key_value>& to_add) {
-
+   auto s = _transaction->Get(opt, key, &result);
+   if (!s.ok()) {
+	  throw fil::exception(put_error_code(), fmt::format("Error while getting key {} : {}", key, s.ToString()));
+   }
+   return result;
 }
 
 std::vector<std::string> kv_rocksdb::transaction::multi_get(const std::vector<std::string>& keys) {
+   std::vector<std::string> results;
+   rocksdb::ReadOptions opt;
+   std::vector<rocksdb::Slice> slicing;
 
+   slicing.reserve(keys.size());
+   for (const auto& key : keys) {
+	  slicing.emplace_back(key);
+   }
+   auto status = _transaction->MultiGet(opt, slicing, &results);
+
+   for (const auto& s : status) {
+	  if (!s.ok()) {
+		 throw fil::exception(put_error_code(), fmt::format("Error while multi getting {} values : {}", keys.size(), s.ToString()));
+	  }
+   }
+   return results;
+}
+
+bool kv_rocksdb::transaction::set(const key_value& to_add) {
+   std::string value;
+   rocksdb::Status s = _transaction->Put(to_add.first, to_add.second);
+   if (!s.ok()) {
+	  throw fil::exception(put_error_code(), fmt::format("Error while inserting key-value : {}-{} : {}", to_add.first, to_add.second, s.ToString()));
+   }
+   return true;
+}
+
+bool kv_rocksdb::transaction::multi_set(const std::vector<key_value>& to_adds) {
+   for (const auto& to_add : to_adds) {
+	  set(to_add);
+   }
+   return true;
 }
 
 kv_rocksdb::kv_rocksdb(const initializer_type& initializer) {
 
-    rocksdb::OptimisticTransactionDB* txn_db;
-    rocksdb::Options option = []() {
+   rocksdb::OptimisticTransactionDB* txn_db;
+   rocksdb::Options option = []() {
 				rocksdb::Options opt;
 				opt.create_if_missing = true;
 				return opt; }();
-    
-    rocksdb::Status status = rocksdb::OptimisticTransactionDB::Open(option, initializer.path_db_file, &txn_db);
 
-    if (!status.ok()) {
-      throw std::runtime_error(fmt::format(FMT_STRING("Couldn't open optimistic RocksDB Database: {}"), status.ToString()));
-    }
+   auto status = rocksdb::OptimisticTransactionDB::Open(option, initializer.path_db_file, &txn_db);
 
-	_db.reset(txn_db);
-  }
+   if (!status.ok()) {
+	  throw std::runtime_error(fmt::format(FMT_STRING("Couldn't open optimistic RocksDB Database: {}"), status.ToString()));
+   }
 
+   _db.reset(txn_db);
 }
+
+}// namespace fil

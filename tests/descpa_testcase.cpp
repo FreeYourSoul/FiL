62953b3ae38f6c8f4ecc831bf96c72b03a8315ae
FyS <ballandFyS@protonmail.com>
2026-03-23 23:30:05 +0100

    descpa : add + concatenation

--- tests/descpa_testcase.cpp
+++ tests/descpa_testcase.cpp
@@ -7,9 +7,10 @@
 
 TEST_CASE("descpa basic tests", "[descpa]") {
 
+    fil::descpa::details_::matcher_ctx ctx;
+    ctx.idx.push_back(0);
     SECTION("test char") {
 
-        fil::descpa::details_::matcher_ctx ctx;
         fil::descpa::match_char<'X'> char_x_check;
 
         CHECK(char_x_check.match(ctx, 'X') == fil::descpa::match_result::SUCCESS);
@@ -18,30 +19,62 @@ TEST_CASE("descpa basic tests", "[descpa]") {
     }
     SECTION("test string") {
 
-        fil::descpa::details_::matcher_ctx ctx;
         fil::descpa::match_string<fil::descpa::fixed_string {"CHOCOBO"}> string_check;
 
         SECTION("Success") {
+            CHECK(ctx.idx.size() == 1);
+            CHECK(ctx.idx.back() == 0);
             CHECK(string_check.match(ctx, 'C') == fil::descpa::match_result::CONTINUE);
+            CHECK(ctx.idx.size() == 1);
+            CHECK(ctx.idx.back() == 1);
             CHECK(string_check.match(ctx, 'H') == fil::descpa::match_result::CONTINUE);
+            CHECK(ctx.idx.size() == 1);
+            CHECK(ctx.idx.back() == 2);
             CHECK(string_check.match(ctx, 'O') == fil::descpa::match_result::CONTINUE);
+            CHECK(ctx.idx.size() == 1);
+            CHECK(ctx.idx.back() == 3);
             CHECK(string_check.match(ctx, 'C') == fil::descpa::match_result::CONTINUE);
+            CHECK(ctx.idx.size() == 1);
+            CHECK(ctx.idx.back() == 4);
             CHECK(string_check.match(ctx, 'O') == fil::descpa::match_result::CONTINUE);
+            CHECK(ctx.idx.size() == 1);
+            CHECK(ctx.idx.back() == 5);
             CHECK(string_check.match(ctx, 'B') == fil::descpa::match_result::CONTINUE);
+            CHECK(ctx.idx.size() == 1);
+            CHECK(ctx.idx.back() == 6);
             CHECK(string_check.match(ctx, 'O') == fil::descpa::match_result::SUCCESS);
+            CHECK(ctx.idx.back() == 7);
         }
-        SECTION("Failure start") { CHECK(string_check.match(ctx, 'c') == fil::descpa::match_result::FAILURE); }
+
+        SECTION("Failure start") {
+            CHECK(!ctx.idx.empty());
+            CHECK(ctx.idx.back() == 0);
+            CHECK(string_check.match(ctx, 'c') == fil::descpa::match_result::FAILURE);
+            CHECK(!ctx.idx.empty());
+            CHECK(ctx.idx.back() == 0);
+        }
+
         SECTION("Failure then restart") {
             CHECK(string_check.match(ctx, 'c') == fil::descpa::match_result::FAILURE);
+            CHECK(!ctx.idx.empty());
+            CHECK(ctx.idx.back() == 0);
             CHECK(string_check.match(ctx, 'C') == fil::descpa::match_result::CONTINUE);
+            CHECK(!ctx.idx.empty());
+            CHECK(ctx.idx.back() == 1);
         }
+
         SECTION("Failure middle way") {
+            CHECK(ctx.idx.back() == 0);
             CHECK(string_check.match(ctx, 'C') == fil::descpa::match_result::CONTINUE);
+            CHECK(ctx.idx.back() == 1);
             CHECK(string_check.match(ctx, 'H') == fil::descpa::match_result::CONTINUE);
+            CHECK(ctx.idx.back() == 2);
             CHECK(string_check.match(ctx, 'O') == fil::descpa::match_result::CONTINUE);
+            CHECK(ctx.idx.back() == 3);
             CHECK(string_check.match(ctx, 'C') == fil::descpa::match_result::CONTINUE);
+            CHECK(ctx.idx.back() == 4);
             CHECK(string_check.match(ctx, 'X') == fil::descpa::match_result::FAILURE);
-            CHECK(string_check.match(ctx, 'O') == fil::descpa::match_result::FAILURE); // failure -- must restart from a C
+            CHECK(ctx.idx.back() == 0);
 
             SECTION("success afterwards") {
                 CHECK(string_check.match(ctx, 'C') == fil::descpa::match_result::CONTINUE);
@@ -51,21 +84,37 @@ TEST_CASE("descpa basic tests", "[descpa]") {
                 CHECK(string_check.match(ctx, 'O') == fil::descpa::match_result::CONTINUE);
                 CHECK(string_check.match(ctx, 'B') == fil::descpa::match_result::CONTINUE);
                 CHECK(string_check.match(ctx, 'O') == fil::descpa::match_result::SUCCESS);
+                CHECK(ctx.idx.back() == 7);
             }
         }
     }
+
     SECTION("test composition") {
-
-        fil::descpa::details_::matcher_ctx ctx;
-
         SECTION("compose 3 char") {
 
             SECTION("direct instantiation") {
                 fil::descpa::tuple_rule<fil::descpa::match_char<'L'>, fil::descpa::match_char<'O'>, fil::descpa::match_char<'L'>> composed;
 
+                CHECK(ctx.idx.back() == 0);
                 CHECK(composed.match(ctx, 'L') == fil::descpa::match_result::CONTINUE);
+                CHECK(ctx.idx.back() == 1);
+                CHECK(composed.match(ctx, 'O') == fil::descpa::match_result::CONTINUE);
+                CHECK(ctx.idx.back() == 2);
+                CHECK(composed.match(ctx, 'L') == fil::descpa::match_result::SUCCESS);
+                CHECK(ctx.idx.back() == 3);
             }
-            SECTION("+ instantiation") {}
+            SECTION("+ instantiation") {
+                constexpr auto composed =
+                    fil::descpa::match_char<'L'> {} + fil::descpa::match_char<'O'> {} + fil::descpa::match_char<'L'> {};
+
+                CHECK(ctx.idx.back() == 0);
+                CHECK(composed.match(ctx, 'L') == fil::descpa::match_result::CONTINUE);
+                CHECK(ctx.idx.back() == 1);
+                CHECK(composed.match(ctx, 'O') == fil::descpa::match_result::CONTINUE);
+                CHECK(ctx.idx.back() == 2);
+                CHECK(composed.match(ctx, 'L') == fil::descpa::match_result::SUCCESS);
+                CHECK(ctx.idx.back() == 3);
+            }
         }
     }
 }

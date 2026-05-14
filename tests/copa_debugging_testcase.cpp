01494d69e1e4edbb438a1f22bff938aa352b01b5
FyS <ballandFyS@protonmail.com>
2026-05-14 12:08:29 +0200

    copa : improve documentation (add gst

---
+++ tests/copa_debugging_testcase.cpp
@@ -0,0 +1,11 @@
+// Dual Licensing Either:
+// - AGPL
+// or
+// - Subscription license for commercial usage (without requirement of licensing propagation).
+//   please contact ballandfys@protonmail.com for additional information about this subscription commercial licensing.
+//
+// Created by fys on 14.05.26. @Copyright Licensing 2022-2026
+//
+// In the case no license has been purchased for the use (modification or distribution in any way) of the software stack
+// the APGL license is applying.
+//
